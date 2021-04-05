020458cd90f3cd63d4bd641861ad96329d46c00f
YuliaS <ysolodukha@gmail.com>
2021-04-05 17:10:02 +0300

    final version

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// GET https://api.ratesapi.io/api/2010-01-12?symbols=USD,GBP HTTP/2
-
-// Для получения данных по курсу валют, вам нужно запросить 
-// данные по валютной паре, добавив к URL-адресу два сокращения этих валют 
-// (без пробелов и разделителей). Пример для пары USD и RUB:
-// https://api.ratesapi.io/api/latest?base=USD&symbols=RUB
-// В ответ приходит JSON с объектом. В поле rates этого объекта лежит 
-// объект пар ключ-значение. По ключу RUB будет находится число — текущий курс на данный момент.
-// Калькулятор с помощью элементов управления позволяет пользователю 
-// выбрать две валюты и сумму конвертации. После выбора калькулятор отображает кросс-курс и итоговую сумму. 
-// Предусмотреть обработку ошибок загрузки.
 
 let currencyFrom = 'RUB';
 let currencyTo = 'USD';
@@ -29,12 +18,14 @@ let newPar = document.querySelector('.new-p');
 const loadingBlock = document.querySelector(".loading-block");
 let flagAPI = false;
 const regTest = /^[0-9]*[.,]?[0-9]+$/;
+const choiceLeft = document.querySelectorAll('.choice-left');
+const choiceRight = document.querySelectorAll('.choice-right');
 
 
 // ~~~~~~~~~~~~~~~~~~LEFT BLOCK!!!!!!!
 
 currencyFromChoice.addEventListener('click', event => {
-    document.querySelectorAll('.choice-left').forEach(el =>
+    choiceLeft.forEach(el =>
         el.classList.remove('active'))
     event.target.classList.add('active')
     if (event.target.tagName.toLowerCase() === 'button') {
@@ -44,22 +35,20 @@ currencyFromChoice.addEventListener('click', event => {
     } else {
         return;
     }
-    console.log(currencyFrom)
 })
 
 selectFrom.addEventListener('change', event => {
-    document.querySelectorAll('.choice-left').forEach(el =>
+    choiceLeft.forEach(el =>
         el.classList.remove('active'))
     event.target.classList.add('active')
     currencyFrom = document.querySelector('.other-currencies-from').value;
     getData();
-    console.log(currencyFrom)
 })
 
 // ~~~~~~~~~~~~~~~~~~~RIGHT BLOCK!!!!!!!!
 
 currencyToChoice.addEventListener('click', event => {
-    document.querySelectorAll('.choice-right').forEach(el =>
+    choiceRight.forEach(el =>
         el.classList.remove('active'))
     event.target.classList.add('active')
     if (event.target.tagName.toLowerCase() === 'button') {
@@ -68,16 +57,14 @@ currencyToChoice.addEventListener('click', event => {
     } else {
         return;
     }
-    console.log(currencyTo)
 })
 
 selectTo.addEventListener('change', event => {
-    document.querySelectorAll('.choice-right').forEach(el =>
+    choiceRight.forEach(el =>
         el.classList.remove('active'))
     event.target.classList.add('active')
     currencyTo = document.querySelector('.other-currencies-to').value;
     getData();
-    console.log(currencyTo)
 })
 
 // ~~~~~~~~~~~~~~~~~~~~MIDDLE ARROW!!! добавить изменение класса
@@ -91,23 +78,21 @@ document.querySelector('.middle-arrow').addEventListener('click', event => {
     selectFrom.getElementsByTagName('option')[indexSelectRight].selected = 'selected';
     selectTo.getElementsByTagName('option')[indexSelectLeft].selected = 'selected';
 
-    document.querySelectorAll('.choice-left').forEach(el => 
+    choiceLeft.forEach(el => 
         el.classList.remove('active'))
     document.querySelectorAll('.button-left').forEach(el => {
         if (el.innerHTML === currencyFrom) {
-            console.log(`I'm in if......`)
             el.classList.add('active');
         } 
     })
    if (document.querySelectorAll('.left-block button.active').length === 0) {
         selectFrom.classList.add('active');
    }
-    document.querySelectorAll('.choice-right').forEach(el => {
+    choiceRight.forEach(el => {
         el.classList.remove('active')
     })
     document.querySelectorAll('.button-right').forEach(el => {
         if (el.innerHTML === currencyTo) {
-            console.log(`I'm in if......`)
             el.classList.add('active');
         } 
     })
@@ -130,12 +115,9 @@ const getData = async () => {
         const response = await fetch(`https://api.ratesapi.io/api/latest?base=${currencyFrom}&symbols=${currencyTo}`);
         const data = await response.json();
         courseLeftToRight = data.rates[currencyTo];
-        console.log(courseLeftToRight);
         const response2 = await fetch(`https://api.ratesapi.io/api/latest?base=${currencyTo}&symbols=${currencyFrom}`);
         const data2 = await response2.json();
         courseRightToLeft = data2.rates[currencyFrom];
-        console.log(courseRightToLeft);
-        console.log(data, data2);
         flagAPI = true;
         console.log(`I work with API...`);
         loadingBlock.style.display = 'none';
@@ -146,7 +128,7 @@ const getData = async () => {
     }
     currentExchangeLeft.innerHTML = `1 ${currencyFrom} = ${courseLeftToRight} ${currencyTo}`;
     currentExchangeRight.innerHTML = `1 ${currencyTo} = ${courseRightToLeft} ${currencyFrom}`;
-    inputToCalcLeft(inputLeft, inputRight, courseLeftToRight);
+    inputToCalc(inputLeft, inputRight, courseLeftToRight);
 }
 
 // ~~~~~~~~~~~~~~~~~~~~INPUT!!!!
@@ -154,12 +136,11 @@ const getData = async () => {
 // ~~~~~~~~~~~~LEFT!!!
 
 inputLeft.addEventListener('input', event => {
-    inputToCalcLeft(event.target, inputRight, courseLeftToRight)
+    inputToCalc(event.target, inputRight, courseLeftToRight)
 })
 
-function inputToCalcLeft(el, secEl, course) {
+function inputToCalc(el, secEl, course) {
     let stringValue = String(el.value);
-    console.log(stringValue, '1')
     stringValue = stringValue
         .replace(/([^0-9.,])/g, '')
         .replace(/,/g, '.');
@@ -169,9 +150,7 @@ if (pointIndex !== -1) {
     const afterPoint = stringValue.substring(pointIndex + 1);
     stringValue = `${pointAndBefore}${afterPoint.replace(/\./g, '')}`;
 }
-    console.log(stringValue)
     el.value = stringValue;
-    console.log(amount, 'bu')
     secEl.value = (Number(stringValue) * course).toFixed(4);
 }
 
@@ -179,7 +158,7 @@ if (pointIndex !== -1) {
 // ~~~~~~~~~~RIGHT!!!
 
 inputRight.addEventListener('input', event => {
-    inputToCalcLeft(event.target, inputLeft, courseRightToLeft)
+    inputToCalc(event.target, inputLeft, courseRightToLeft)
 })
 
 // ~~~~~~~~~~~~~~~~~Loader!!!
